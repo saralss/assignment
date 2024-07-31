c4ed112251d43dc873b82ef58923bb3ac8ed9066
saralss <sharmasaral746@gmail.com>
2024-07-31 17:05:15 +0545

    added to array

---
+++ javascript/array/level1.js
@@ -0,0 +1,8 @@
+const arr = [3,4,5]
+//Q1 
+// find the length of the array
+//expected output is 3
+console.log(arr.length)
+//reverse the string 
+//expected output [5,4,3]
+console.log(arr.reverse())
